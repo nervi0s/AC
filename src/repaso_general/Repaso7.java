b5b63aced8d306c43e2a394b1ac19300c17c82b6
nervi0s <nervi0s@protonmail.com>
2020-11-10 21:07:43 +0100

    Close Scanner

--- src/repaso_general/Repaso7.java
+++ src/repaso_general/Repaso7.java
@@ -6,14 +6,20 @@ import java.util.Scanner;
 public class Repaso7 {
 
 	public static void main(String[] args) {
+
 		int intentos = 1;
 		String continuar = "SI";
 		int randomNumber = getRandomNum(0, 1000);
-		System.out.println(randomNumber);
+
+		System.out.println(randomNumber); // Número secreto
+
 		int userNumber;
+		Scanner sc = new Scanner(System.in);
+
 		do {
 			System.out.println("Adivima un número entre 1 y el 1000");
-			userNumber = new Scanner(System.in).nextInt();
+			userNumber = sc.nextInt();
+
 			if (userNumber < randomNumber) {
 				System.out.println("El número es mayor");
 			} else if (userNumber > randomNumber) {
@@ -23,12 +29,13 @@ public class Repaso7 {
 				System.out.println(randomNumber);
 				System.out.println("Has adivinado el número en el intento: " + intentos);
 				System.out.println("Quieres continuar? (SI/NO): ");
-				continuar = new Scanner(System.in).nextLine();
+				continuar = sc.next();
 				intentos = 0;
 			}
 			intentos++;
 		} while (userNumber != randomNumber && continuar.equals("SI"));
 
+		sc.close();
 		System.out.println("Fin del programa");
 
 	}
